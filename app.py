dd7c5425a5c9fdad7ccc2c19e204aa44f80082b7
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-05 20:41:26 -0400

    feat: delete redirect stuff

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 import yaml
 
-from urllib.parse import urlparse, urljoin, urlunparse, urlencode
 from pathlib import Path
 
 with open("config.yml", "r", encoding="utf8") as f:
@@ -24,36 +23,6 @@ app.mount(f"{prefix}/dist", StaticFiles(directory=dist_dir), name="dist")
 
 @app.get(f"{prefix}/{{path:path}}")
 async def serve_spa(request: Request, path: str):
-    if path.startswith("sonolus/"):
-        if config["redirect-sonolus"]["enabled"]:
-            redirect_base = config["redirect-sonolus"]["path"]
-            target_scheme = "https" if config["https"] else "http"
-            parsed_url = urlparse(redirect_base)
-
-            query_params = request.query_params
-            query_string = urlencode(query_params)
-
-            if parsed_url.scheme and parsed_url.netloc:
-                new_url = urljoin(redirect_base, path)
-                if config["redirect-sonolus"]["add-trailing-slash"]:
-                    new_url += "/"
-                if query_string:
-                    new_url = f"{new_url}?{query_string}"
-                parsed_url = urlparse(new_url)
-                parsed_url = parsed_url._replace(scheme=target_scheme)
-                full_redirect_url = urlunparse(parsed_url)
-            else:
-                base_url = str(request.base_url)
-                full_url = urljoin(base_url, redirect_base)
-                full_url = urljoin(full_url, path)
-                if config["redirect-sonolus"]["add-trailing-slash"]:
-                    full_url += "/"
-                if query_string:
-                    full_url = f"{full_url}?{query_string}"
-                parsed_url = urlparse(full_url)
-                parsed_url = parsed_url._replace(scheme=target_scheme)
-                full_redirect_url = urlunparse(parsed_url)
-            return RedirectResponse(full_redirect_url, status_code=302)
     file_path = dist_dir / path
     if not file_path.exists():
         file_path = dist_dir / "index.html"
